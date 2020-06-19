3509aa186e462390073c957c0e2345d5661e7338
RadioJackie <jacobpeters628@gmail.com>
2020-06-18 22:31:59 -0700

    Made alot of changes to everything and got confused

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -150,18 +150,3 @@ $noteText.on("keyup", handleRenderSaveBtn);
 // Gets and renders the initial list of notes
 getAndRenderNotes();
 
-// The route path for the html files Notes and Index
-
-
-const path = require("path")
-
-module.exports = function (app){
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-};
